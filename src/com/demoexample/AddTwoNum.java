53d7886d9f583d31bb10ad438ebbe57e14b31e12
Amar-11 <kumarak4896@gmail.com>
2021-05-03 13:40:45 +0530

    first commit............

---
+++ src/com/demoexample/AddTwoNum.java
@@ -0,0 +1,13 @@
+package com.demoexample;
+
+public class AddTwoNum {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+int a =20;
+int b = 50;
+int c=a+b;
+System.out.println(c);
+	}
+
+}
---
+++ src/com/demoexample/Amar.java
@@ -0,0 +1,10 @@
+package com.demoexample;
+
+public class Amar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.print("amar");
+	}
+
+}
