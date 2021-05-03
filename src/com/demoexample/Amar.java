f00a8ec6c5dab66afa41b2981ee33e698220e624
Amar-11 <kumarak4896@gmail.com>
2021-05-03 18:18:31 +0530

    i did some in good way

--- src/com/demoexample/Amar.java
+++ src/com/demoexample/Amar.java
@@ -4,7 +4,10 @@ public class Amar {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.print("amar");
+System.out.println("this is amar");
+System.out.println("i am studet ");
+System.out.println("now a days i am trying to learn many thing ");
+System.out.println("so thats something about me ");
 	}
 
 }
