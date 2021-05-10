12de43204cd59913790af8ec59c6b9509641ab9b
ashwini sharma <ashwini.swe@gmail.com>
2021-05-10 19:03:46 +0530

    implements Thread

---
+++ src/thread/ImplementsThread.java
@@ -0,0 +1,16 @@
+package thread;
+
+public class ImplementsThread implements Runnable {
+public void run() {
+	for(int i=0;i<9;i++) {
+		System.out.println("child class : " +i);
+	}
+}
+	public static void main(String[] args) {
+		ImplementsThread t = new ImplementsThread();
+		Thread t1 = new Thread();
+		t1.start();
+
+	}
+
+}
